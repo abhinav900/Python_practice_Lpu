823fe9c6e8527270d9446dc2ed16dd0b13a3639c
abhinav900 <abhinavsingh30795@gmail.com>
2022-10-30 22:09:10 +0530

    greater number

---
+++ greatestNumber.py
@@ -0,0 +1,14 @@
+#take two inputs from user (int/float)
+#print the greatest number
+
+num1 =int(input("first number is :"))
+
+num2 = int(input("second number is :"))
+
+
+if num1>num2:
+    print("the num1 is greatest")
+
+else:
+    num1<num2
+    print("the num2 is youngest")
