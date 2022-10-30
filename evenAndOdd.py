04c4f932bcd374e087dc563d7ab9b2d547a70c07
abhinav900 <abhinavsingh30795@gmail.com>
2022-10-30 22:06:37 +0530

    odd and even

---
+++ evenAndOdd.py
@@ -0,0 +1,9 @@
+# take user input (input)
+# print the given number is odd or even
+
+num =int(input("the number is:"))
+
+if num%2==0:
+    print("the number is even")
+else:
+    print("the number is odd")
