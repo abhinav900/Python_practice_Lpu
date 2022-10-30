a394cbc28fe938ffb1b4dafc216cb2c5869111aa
abhinav900 <abhinavsingh30795@gmail.com>
2022-10-30 22:16:38 +0530

    uses of loops

---
+++ loop/loopExample.py
@@ -0,0 +1,39 @@
+from re import A
+
+
+cars=["thar","ford","bmw","bullet","tata"]
+#for i in cars:
+ #   if i=="bullet":
+  #      break
+   # print(i)
+
+
+
+#for i in cars:
+ #   if i=="bmw":
+  #      continue
+   # print(i)
+
+
+
+
+
+
+#for a in "hello world":
+ #   print(a)
+
+
+
+#for i in range(0,10):
+ #   print(i)
+
+#for a in range(0,100,2):
+ #   print(a)
+
+for b in range(6):
+    if b==3:
+        
+        print(b)
+        break
+    else:
+        print("loop over")
