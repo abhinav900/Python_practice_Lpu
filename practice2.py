e45dc3276ee966c42a46609fe6116d475c8b7ac7
abhinav900 <abhinavsingh30795@gmail.com>
2022-10-30 22:12:33 +0530

    greater number

---
+++ practice2.py
@@ -0,0 +1,24 @@
+#a =9
+
+#b =6
+
+#if a>b:
+ #   print("we are in if block")
+
+#else:
+ #   print("we are in else block")
+
+
+# question practice
+
+
+
+x =input("first number:")
+
+y = input("second number")
+
+if x<y:
+    print("second number is greater",y)
+
+else:
+    print("first number is greater",x)
