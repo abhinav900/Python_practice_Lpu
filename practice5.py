884e52eb0ee9998a97e8491cde51178466850c38
abhinav900 <abhinavsingh30795@gmail.com>
2022-11-02 09:39:10 +0530

    question practice

---
+++ practice5.py
@@ -0,0 +1,9 @@
+x = float(input("total number of classes:"))
+
+y = float(input("total attend class:"))
+
+if (y/x)*100>=75:
+    print("eligible for examination")
+
+else:
+    print("not eligible for examination")
