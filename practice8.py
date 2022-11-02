722bb47cb517be2ba95e17f47e065e3c4b147756
abhinav900 <abhinavsingh30795@gmail.com>
2022-11-02 09:42:53 +0530

    if and elif question

---
+++ practice8.py
@@ -0,0 +1,15 @@
+a=200
+
+if a<500:
+    print("a less than 500")
+    if(a>300):
+        print("A is less than 300")
+    elif a==200:
+        print("A is 200")
+    else:
+        print("A is nothing")
+elif a<200:
+    print("A is less than 200")
+else:
+    print("else will print")
+print("hello world")
