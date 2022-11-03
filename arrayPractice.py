9f3f1e57aaf462ae0ab4452909ff11d1c03d10ee
abhinav900 <abhinavsingh30795@gmail.com>
2022-11-03 17:42:09 +0530

    array practice

---
+++ arrayPractice.py
@@ -0,0 +1,23 @@
+a = "Hello World"
+print(a[10])
+b="Abhinav Kumar"     #same data type  is array
+print(b[0])           #different data type is list
+print(b[1])
+print(b[2])
+print(len(a))
+print(len(b))
+#print word from sentence
+sentence="A quick brown fox jumps over the dog"
+if "we" not in  sentence:
+    print("yes it is there ")
+else:
+    print("no")
+
+print(sentence[2:7])
+print(sentence[8:])
+print(sentence[:])
+print(sentence[:9])
+print(sentence[40:])
+
+x="world"
+print(x[-5:-2])
