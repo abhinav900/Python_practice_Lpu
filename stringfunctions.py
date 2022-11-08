ba59198d31d70f3757aab22e8a22e5cba859d9fb
abhinav900 <abhinavsingh30795@gmail.com>
2022-11-08 10:26:57 +0530

    story word finding

---
+++ stringfunctions.py
@@ -0,0 +1,8 @@
+#string functions
+story="once there was a lion in jungle and he saw a deer and wants to kill him but deer saw the lion and run fastly away from the lion"
+print(len(story))
+print(story.endswith("lion"))
+print(story.count("e"))
+print(story.capitalize()) #it makes first letter of story capital if the story strats with small words
+print(story.find("there"))
+print(story.replace("deer","elephant"))
