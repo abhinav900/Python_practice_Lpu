1ff1ca18935f5d37443bc5d7d285ce56b5f13a05
abhinav900 <abhinavsingh30795@gmail.com>
2022-11-08 10:32:15 +0530

    swap the numbers

---
+++ swapFunction.py
@@ -0,0 +1,15 @@
+x =4
+y = 7
+print ("before swap,value of x is:",x)
+print ("before swap,value of y is:",y)
+
+temp = x
+print("value of temp is:",temp)
+x = y
+
+print ("value of x is",x)
+y = temp
+print("the value of y:",y)
+
+print("AFTER SWAP,value of x is ",x)
+print("AFTER SWAP the value of y is",y)
